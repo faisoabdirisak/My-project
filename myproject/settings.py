510897ebad09b8f042590b3dcb397b784485dda2
faisoabdirisak <faisoabdirisak@gmail.com>
2022-05-27 06:46:54 +0300

    add changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URLS=''
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
