f3bbf220de24a58b3767ce7e21b421076865cf64
faisoabdirisak <faisoabdirisak@gmail.com>
2022-06-01 08:22:03 +0300

    add project reviews

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,10 @@ from operator import le
 from django.shortcuts import redirect, render
 from .models import Project, Tag
 from django.contrib.auth.decorators import login_required
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
+from django.contrib import messages
 from .utils import searchProjects, paginateProjects
 
 
@@ -17,8 +18,22 @@ def projects(request):
 
 def project(request, pk):
     projectObj=Project.objects.get(id=pk)
+    form= ReviewForm()
+
+    if request.method == 'POST':
+        form=ReviewForm(request.POST)
+        review=form.save(commit=False)
+        review.project=projectObj
+        review.owner=request.user.profile
+        review.save()
+        projectObj.getVoteCount
+        messages.success(request, 'review was successfully submitted')
+        return redirect('project', pk=projectObj.id)
+
+        # update project vote count
+
     tags=projectObj.tags.all()
-    context={'project':projectObj, 'tags':tags}
+    context={'project':projectObj, 'tags':tags, 'form':form}
     return render(request, 'projects/single-project.html', context)   
 
 @login_required(login_url="login")
